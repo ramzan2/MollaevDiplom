22cddcaa0e58a454bbbbc97a0dfda55a745f9975
ramza <ramzanmollaev7@gmail.com>
2024-04-12 13:42:03 +0300

    Добавьте файлы проекта.

---
+++ MollaevDiplom/WindowFolder/AuthorizationWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MollaevDiplom.WindowFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+      
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        private void placeholderText_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            placeholderText.Visibility = Visibility.Collapsed;
+        }
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            placeholderText.Visibility = Visibility.Collapsed;
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            placeholderText.Visibility= Visibility.Visible;
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void LogInBtn_Click(object sender, RoutedEventArgs e)
+        {
+ 
+        }
+
+        private void LoginTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+         
+        }
+
+        private void PasswordTb_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+           
+        }
+      
+    }
+}
+
---
+++ MollaevDiplom/WindowFolder/MenuDirectorWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MollaevDiplom.WindowFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuDirectorWindow.xaml
+    /// </summary>
+    public partial class MenuDirectorWindow : Window
+    {
+        public MenuDirectorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Employees_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Meetings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Tasks_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Document_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Tasks_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Tasks_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
